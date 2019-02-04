9f27e4bc4a0967f709c01356d027a04ec8a54762
zm17383 <zm17383@josai.ac.jp>
2019-02-04 04:06:41 -0500

    message

---
+++ ex07-8.js
@@ -0,0 +1,7 @@
+function nazo() {
+ if (document.geteIementByid('answer').value='夢') {
+   alert('あたり！');
+ } else {
+   alert('はずれ！');
+ }
+}
---
+++ saikoro.js
@@ -0,0 +1,4 @@
+function getNum() {
+	r = Math.floor(Math.random() * 6) + 1; 
+	document.form1.sai.value = r; 
+}
